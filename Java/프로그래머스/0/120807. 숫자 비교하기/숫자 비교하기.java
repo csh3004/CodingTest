a4a662076b5fe719b359f98a22ff5152b95be90e
hoon <43110196+csh3004@users.noreply.github.com>
2024-09-07 16:23:44 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.03 ms, Memory: 84.8 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int num1, int num2) {
+        int answer = -1;
+        if(num1 == num2) answer = 1;
+        return answer;
+    }
+}
