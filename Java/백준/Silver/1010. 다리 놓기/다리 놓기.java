89009c9ebd82fd037947bb7f8059f5cc89eb84d2
hoon <43110196+csh3004@users.noreply.github.com>
2024-10-06 19:25:51 +0900

    [Silver V] Title: 다리 놓기, Time: 208 ms, Memory: 18100 KB -BaekjoonHub

--- Java/백준/Silver/1010. 다리 놓기/다리 놓기.java
+++ Java/백준/Silver/1010. 다리 놓기/다리 놓기.java
@@ -7,16 +7,14 @@ public class Main {
         int testcase = sc.nextInt();
 
         for (int i = 0; i < testcase; i++) {
-            int N = sc.nextInt();  // 서쪽 사이트 (N)
-            int M = sc.nextInt();  // 동쪽 사이트 (M)
-            System.out.println(combi(M, N));  // 조합 계산 결과 출력
+            int N = sc.nextInt();  
+            int M = sc.nextInt();  
+            System.out.println(combi(M, N));  
         }
         sc.close();
     }
 
-    // 효율적인 조합 계산 방식 (팩토리얼을 사용하지 않음)
     private static long combi(int n, int k) {
-        // C(n, k) = C(n, n - k) -> 더 작은 쪽을 선택해 계산 최적화
         if (k > n - k) {
             k = n - k;
         }
@@ -24,7 +22,7 @@ public class Main {
         long result = 1;
         for (int i = 0; i < k; i++) {
             result *= (n - i);
-            result /= (i + 1);  // 매 단계마다 나눗셈을 해 큰 수를 방지
+            result /= (i + 1);  
         }
 
         return result;
